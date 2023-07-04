1ab4cf898d7f76e550864501a832b5dbc9415f96
ThomMoerman <thomas.moerman.7@gmail.com>
2023-07-04 14:51:00 +0200

    Setup_Singleton

---
+++ Core/DatabaseConnection.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Core;
+
+class DatabaseConnection {
+
+    private static $instance;
+
+    public static function getInstance()
+    {
+        if (!self::$instance) {
+            $dbHost = 'localhost';
+            $dbName = 'cogipdb';
+            $dbUser = 'root';
+            $dbPass = '';
+
+            self::$instance = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPass);
+        }
+
+        return self::$instance;
+    }
+
+}
+
+?>
--- Routes/Routes.php
+++ Routes/Routes.php
@@ -1,6 +1,7 @@
 <?php
-
 namespace App\Routes;
+
+ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
 
 use Bramus\Router\Router;
 use App\Controllers\HomeController;
