12acf1e3499e81152d7cecb8e421f3f90463e0cd
Abel Fortunati <abel.fortunati@gmail.com>
2023-07-04 14:52:58 +0200

    header mixins and base style

---
+++ Resources/Include/header.php
@@ -0,0 +1,13 @@
+<header>
+    <h1 class="header__title">COGIP</h1>
+    <div class="nav header__navbar">
+        <button type="button" class="btn navbar__home_btn">HOME</button>
+        <button type="button" class="btn navbar__invoices_btn">INVOICES</button>
+        <button type="button" class="btn navbar__compagnies_btn">COMPANIES</button>
+        <button type="button" class="btn navbar__contacts_btn">CONTACTS</button>
+    </div>
+    <div class="log header__log">
+        <button type="button" class="btn log__invoices_btn">SIGN UP</button>
+        <button type="button" class="btn log__compagnies_btn">LOGIN</button>
+    </div>
+</header>
--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
-
 require './Resources/Include/header.php';
-
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>COGIP</title>
+    <link rel="stylesheet" href="./public/assets/css/reset.css">    
+<link rel="stylesheet" href="./public/assets/css/header.css">
+</head>
+<body>
+    
+</body>
+</html>
