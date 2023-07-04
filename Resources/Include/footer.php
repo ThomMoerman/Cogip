8ad52e2794c26f16203378556dc888381040e7c8
JodieAddis <jodie.addis.92@gmail.com>
2023-07-04 12:32:43 +0200

    structure footer terminée

---
+++ Resources/Include/footer.php
@@ -0,0 +1,53 @@
+<footer>
+    <section class="footer_top">
+        <div class="footer_top_sitename">
+            <p>cogip</p>
+        </div>
+        <div class="footer_top_information">
+            <div class="footer_information_data">
+                <div class="footer_information_data_adress">
+                    <img src="./public/assets/img/geo-alt-fill.svg" alt="location icon" class="footer_information_data_adress_icon">
+                    <p class="footer_information_data_adress_name">Square des Martyrs, 6000 Charleroi</p>
+                </div>
+                <div class="footer_information_data_number">
+                    <div class="footer_information_data_number_phone">
+                        <img src="./public/assets/img/telephone-fill.svg" alt="phone icon">
+                        <p>(123) 456-7890</p>
+                    </div>
+                    <div class="footer_information_data_number_printer">
+                        <img src="./public/assets/img/printer-fill.svg" alt="printer icon">
+                        <p>(123) 456-7890</p>
+                    </div>
+                </div>
+            </div>
+            <div class="footer_information_social_media">
+                <ul class="footer_information_social_media_list">
+                    <li>Social Media</li>
+                    <li><img src="./public/assets/img/facebook.svg" alt="facebook icon"></li>
+                    <li><img src="./public/assets/img/twitter.svg" alt="twitter icon"></li>
+                    <li><img src="./public/assets/img/linkedin.svg" alt="linkedin icon"></li>
+                    <li><img src="./public/assets/img/youtube.svg" alt="youtube icon"></li>
+                    <li><img src="./public/assets/img/instagram.svg" alt="instagram icon"></li>
+                    <li><img src="./public/assets/img/google.svg" alt="google icon"> +</li>
+                    <li><img src="./public/assets/img/pinterest.svg" alt="pinterest icon"></li>
+                    <li><img src="./public/assets/img/wifi.svg" alt="wifi icon"></li>
+                </ul>
+            </div>
+        </div>
+    </section>
+    <hr>
+    <section class="footer_bottom">
+        <div class="footer_bottom_navbar">
+            <ul class="footer_bottom_navbar_list">
+                <li class="footer_bottom_navbar_list_element">home</li>
+                <li class="footer_bottom_navbar_list_element">invoices</li>
+                <li class="footer_bottom_navbar_list_element">companies</li>
+                <li class="footer_bottom_navbar_list_element">contacts</li>
+                <li class="footer_bottom_navbar_list_element">privacy policy</li>
+            </ul>
+        </div>
+        <div class="footer_bottom_copyright">
+            <p>Copyright © 2022 • COGIP Inc.</p>
+        </div>
+    </section>
+</footer>
