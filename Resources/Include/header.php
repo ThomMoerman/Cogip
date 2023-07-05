5a84d2ed7d9e9840831832665b39c60e7a9aaf07
Abel Fortunati <abel.fortunati@gmail.com>
2023-07-05 12:07:24 +0200

    header media queries

--- Resources/Include/header.php
+++ Resources/Include/header.php
@@ -1,13 +1,13 @@
 <header>
     <h1 class="header__title">COGIP</h1>
     <div class="nav header__navbar">
-        <button type="button" class="btn navbar__home_btn">HOME</button>
-        <button type="button" class="btn navbar__invoices_btn">INVOICES</button>
-        <button type="button" class="btn navbar__compagnies_btn">COMPANIES</button>
-        <button type="button" class="btn navbar__contacts_btn">CONTACTS</button>
+        <button type="button" class="btn navbar__home_btn home_link">HOME</button>
+        <button type="button" class="btn navbar__invoices_btn invoices_link">INVOICES</button>
+        <button type="button" class="btn navbar__compagnies_btn companies_link">COMPANIES</button>
+        <button type="button" class="btn navbar__contacts_btn contacts_link">CONTACTS</button>
     </div>
     <div class="log header__log">
-        <button type="button" class="btn log__invoices_btn">SIGN UP</button>
-        <button type="button" class="btn log__compagnies_btn">LOGIN</button>
+        <button type="button" class="btn log__signup_btn signup_link">SIGN UP</button>
+        <button type="button" class="btn log__login_btn login_link">LOGIN</button>
     </div>
 </header>
