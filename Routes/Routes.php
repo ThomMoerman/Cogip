cb36f5a4e13eca40336080e5ed17560ace04c63a
Alphit7 <jacobs.sylvain@gmail.com>
2023-07-06 10:04:19 +0200

    fixed Thomas' mistake

--- Routes/Routes.php
+++ Routes/Routes.php
@@ -2,13 +2,16 @@
 namespace App\Routes;
 
 use App\Controllers\CompanyController;
+use App\Controllers\ContactController;
+use App\Controllers\InvoiceController;
+use Bramus\Router\Router;
+use App\Controllers\HomeController;
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-use Bramus\Router\Router;
-use App\Controllers\HomeController;
+
 
 $router = new Router();
 // $routerCompany = new Router();
@@ -21,12 +24,12 @@ $router->get('/companies', function () {
     (new CompanyController)->index();
 });
 
-$router->get('/contacts', function(){
+$router->get('/contacts', function () {
     (new ContactController)->index();
 });
 
-$router->get('/invoices', function (){
-    (new InvoicesController)->index();
+$router->get('/invoices', function () {
+    (new InvoiceController)->index();
 });
 
 $router->run();
