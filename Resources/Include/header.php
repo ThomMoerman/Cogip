6164c3267caddb9031203e03d953e7b09931b691
Abel Fortunati <abel.fortunati@gmail.com>
2023-07-07 15:48:28 +0200

    heading color

--- Resources/Include/header.php
+++ Resources/Include/header.php
@@ -1,14 +1,14 @@
 <header>
     <h1 class="header__title">COGIP</h1>
     <div class="nav header__navbar">
-        <a type="button" href="/" class="btn navbar__home_btn home_link">HOME</a>
-        <a type="button" href="/invoices" class="btn navbar__invoices_btn invoices_link">INVOICES</a>
-        <a type="button" href="/companies" class="btn navbar__compagnies_btn companies_link">COMPANIES</a>
-        <a type="button" href="/contacts" class="btn navbar__contacts_btn contacts_link">CONTACTS</a>
+        <a type="link" href="/" class="btn navbar__home_btn home_link">HOME</a>
+        <a type="link" href="/invoices" class="btn navbar__invoices_btn invoices_link">INVOICES</a>
+        <a type="link" href="/companies" class="btn navbar__compagnies_btn companies_link">COMPANIES</a>
+        <a type="link" href="/contacts" class="btn navbar__contacts_btn contacts_link">CONTACTS</a>
     </div>
     <div class="log header__log">
-        <button type="button" class="btn log__signup_btn signup_link">SIGN UP</button>
-        <button type="button" class="btn log__login_btn login_link">LOGIN</button>
+        <a type="link"  class="btn log__signup_btn signup_link">SIGN UP</a>
+        <a type="link" class="btn log__login_btn login_link">LOGIN</a>
     </div>
 
 </header>
