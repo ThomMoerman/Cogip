b978400ac03f42ae910852459de8ae26ba813bf5
Alphit7 <jacobs.sylvain@gmail.com>
2023-07-07 11:07:05 +0200

    contacts

--- Controllers/ContactController.php
+++ Controllers/ContactController.php
@@ -8,37 +8,61 @@ class ContactController extends Controller
 {
     public function index()
     {
-        // Création des instances des modèles
+        // Récupérer le numéro de page à afficher (par exemple, depuis une requête GET)
+        $page = isset($_GET['page']) ? intval($_GET['page']) : 1;
+
+        // Définir le nombre d'enregistrements par page
+        $perPage = 10;
+
+        // Création de l'instance du modèle
         $contactModel = new Contact();
 
-        // Récupérer les 5 derniers enregistrements de la table 'companies'
-        $contacts = $contactModel->getContacts();
+        // Récupérer le nombre total d'enregistrements dans la table 'contacts'
+        $totalRecords = $contactModel->getTotalContactCount();
+
+        // Calculer le nombre total de pages
+        $totalPages = ceil($totalRecords / $perPage);
+
+        // Vérifier si la page demandée dépasse le nombre total de pages
+        if ($page > $totalPages) {
+            // Rediriger vers la dernière page si la page demandée est invalide
+            header('Location: /Contacts?page=' . $totalPages);
+            exit();
+        }
+
+        // Calculer l'offset pour la requête SQL
+        $offset = ($page - 1) * $perPage;
+
+        // Récupérer les enregistrements pour la page demandée
+        $contacts = $contactModel->getPaginatedContacts($offset, $perPage);
 
         // Passer les données aux vues correspondantes
         return $this->view('contacts', [
             'name' => 'Cogip',
             'contacts' => $contacts,
+            'totalPages' => $totalPages,
+            'currentPage' => $page
         ]);
     }
     public function show($id)
     {
         // Récupérez les informations de l'entreprise depuis la base de données en utilisant l'ID
-        $contactModel = new Contact();
-        $contact = $contactModel->find($id);
+        $ContactModel = new Contact();
+        $Contact = $ContactModel->find($id);
 
         // Vérifiez si l'entreprise existe
-        if (!$contact) {
-            // alert('The choosen contact doesn\'t exists');
+        if (!$Contact) {
+            // alert('The choosen Contact doesn\'t exists');
         }
 
         // Préparez les données à envoyer à la vue
         $data = [
-            'ref' => $contact->ref,
-            'company' => $contact->company,
-            'created_at' => $contact->created_at
+            'ref' => $Contact->ref,
+            'Contact' => $Contact->Contact,
+            'created_at' => $Contact->created_at
         ];
 
         // Renvoyez les données à la vue appropriée pour l'affichage
-        return $this->view('show_contact', $data);
+        return $this->view('show_Contact', $data);
     }
 }
--- Resources/views/contacts.php
+++ Resources/views/contacts.php
@@ -8,8 +8,7 @@
     <link href="assets/css/header.css" rel="stylesheet" type="text/css">
     <link href="assets/css/footer.css" rel="stylesheet" type="text/css">
     <link href="assets/css/table.css" rel="stylesheet" type="text/css">
-    <script rel='javascript' src="assets/js/searchfilter.js" defer></script>
-    <title>Companies</title>
+    <title>Contacts</title>
 </head>
 
 <body>
@@ -17,34 +16,38 @@
     require '../Resources/Include/header.php'
         ?>
     <main>
-        <section class="container section">
-
-            <h3>All companies</h3>
-
+        <section class="container">
             <!-- Afficher le tableau des entreprises -->
-            <table id='list_table'>
+            <table>
                 <!-- En-têtes de colonne -->
                 <thead>
                     <tr>
-                        <th>ID</th>
-                        <th>Nom</th>
-                        <th>Pays</th>
-                        <th>Type</th>
+                        <th>Name</th>
+                        <th>Phone</th>
+                        <th>Mail</th>
+                        <th>Company</th>
+                        <th>Created at</th>
+
                     </tr>
                 </thead>
                 <!-- Données -->
                 <tbody>
-                    <?php foreach ($companies as $company): ?>
+                    <?php foreach ($contacts as $contact): ?>
                         <tr>
                             <td>
-                                <?php echo $company['id']; ?>
-                            </td>
-                            <td><a href="/companies/<?php echo $company['id']; ?>"><?php echo $company['name']; ?></a></td>
-                            <td>
-                                <?php echo $company['country']; ?>
+                                <?php echo $contact['name']; ?>
                             </td>
                             <td>
-                                <?php echo $company['type_name']; ?>
+                                <?php echo $contact['phone']; ?>
+                            </td>
+                            <td>
+                                <?php echo $contact['email']; ?>
+                            </td>
+                            <td>
+                                <?php echo $contact['company_name']; ?>
+                            </td>
+                            <td>
+                                <?php echo $contact['created_at']; ?>
                             </td>
                         </tr>
                     <?php endforeach; ?>
@@ -55,7 +58,7 @@
             <div class="pagination">
                 <?php if ($currentPage > 1): ?>
                     <!-- Afficher la flèche précédente si ce n'est pas la première page -->
-                    <a href="/companies?page=<?php echo ($currentPage - 1); ?>" class="arrow">← Previous</a>
+                    <a href="/contacts?page=<?php echo ($currentPage - 1); ?>" class="arrow">← Previous</a>
                 <?php endif; ?>
 
                 <?php for ($i = 1; $i <= $totalPages; $i++): ?>
@@ -66,13 +69,13 @@
                         </span>
                     <?php else: ?>
                         <!-- Afficher les autres pages comme des liens -->
-                        <a href="/companies?page=<?php echo $i; ?>"><?php echo $i; ?></a>
+                        <a href="/contacts?page=<?php echo $i; ?>"><?php echo $i; ?></a>
                     <?php endif; ?>
                 <?php endfor; ?>
 
                 <?php if ($currentPage < $totalPages): ?>
                     <!-- Afficher la flèche suivante si ce n'est pas la dernière page -->
-                    <a href="/companies?page=<?php echo ($currentPage + 1); ?>" class="arrow">Next →</a>
+                    <a href="/contacts?page=<?php echo ($currentPage + 1); ?>" class="arrow">Next →</a>
                 <?php endif; ?>
             </div>
         </section>
