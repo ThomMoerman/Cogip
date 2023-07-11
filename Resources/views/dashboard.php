b4c1435af98672d5f8339a91ba3a4ac7d6103684
Abel Fortunati <abel.fortunati@gmail.com>
2023-07-11 16:12:32 +0200

    sticky navbar

--- Resources/views/dashboard.php
+++ Resources/views/dashboard.php
@@ -3,9 +3,54 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="assets/css/reset.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/dashboard_nav.css" rel="stylesheet" type="text/css">
     <title>Dashboard</title>
+    
 </head>
 <body>
+    <section id="dashboard__navbar">
+        <div class="navbar__admin_block">
+            <img src="assets/img/Avatar_two.svg" alt="profile pic">
+            <h3>John Doe</h3>
+        </div>
+        <hr>
+        <div class="navbar__buttons">
+            <button class="nav btn_dashboard">Dashboard</button>
+            <button class="nav btn_invoices">Invoices</button>
+            <button class="nav btn_companies">Companies</button>
+            <button class="nav btn_contacts">Contacts</button>
+        </div>
+    </section>
+    <main>
+            <section class="dashboard__header" >
+            <div class="header_name"></div>
+            <h2>Dashboard</h2>
+            <p>dashboard/</p>
+            <div class="header_message">
+            <h3>Welcome back Henry!</h3>
+            <p>You can here add an invoice, a company and some contacts</p>
+            <img src="" alt="">
+            </div>
+            </div>
+            <section class="dashboard__data">
+                <article class="data" id="statistics">
+                    <h4>Statistics</h4>
+                    <p class="stat total_invoices">245 Invoices</p>
+                    <p class="stat total_contacts">245 Contacts</p>
+                    <p class="stat total_companies">245 Companies</p>
+                </article>
+                <article class="data_table" id="invoices">
+                    <h4>Last Invoices</h4>
+                </article>
+                <article class="data_table" id="contacts">
+                    <h4>Last Contacts</h4>
+                </article>
+                <article class="data_table" id="companies">
+                    <h4>Last Companies</h4>
+                </article>
+            </section>
+    </main>
     
 </body>
 </html>
