ad372d618c06654eb83ca2dec48dc795fba6c86b
ThomMoerman <thomas.moerman.7@gmail.com>
2023-07-11 14:09:55 +0200

    Fix Link

--- Resources/Include/header.php
+++ Resources/Include/header.php
@@ -6,10 +6,6 @@
         <a type="link" href="/companies" class="btn navbar__compagnies_btn companies_link">COMPANIES</a>
         <a type="link" href="/contacts" class="btn navbar__contacts_btn contacts_link">CONTACTS</a>
     </div>
-    <div class="log header__log">
-        <a type="link"  class="btn log__signup_btn signup_link">SIGN UP</a>
-        <a type="link" href="/login" class="btn log__login_btn login_link">LOGIN</a>
-    </div>
     <?php
     session_start();
     // Vérifier si l'utilisateur est connecté
