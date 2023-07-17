83a65f3861a6cd17985b6f77e2339107ac45f102
JodieAddis <jodie.addis.92@gmail.com>
2023-07-17 17:10:22 +0200

    Récupération design

---
+++ Resources/Include/header_dashboard.php
@@ -0,0 +1,17 @@
+<header>
+    <section class="header__section">
+        <div class="section__page__name">
+            <h1>Dashboard</h1>
+            <p>dashbord/</p>
+        </div>
+        <div class="section__welcome__client">
+            <div class="welcome__client__text">
+                <h2>Welcome back Henry</h2>
+                <p>You can here add an invoice, a company and some contacts</p>
+            </div>
+            <div class="welcome__client__image">
+                <img src="../assets/img/dashboard_illustration.svg" alt="Image display a man working on computer">
+            </div>
+        </div>
+    </section>
+</header>
