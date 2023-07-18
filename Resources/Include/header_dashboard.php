04c58ccfc7c2470cbfe3616d5335ef7235eb66c9
Alphit7 <jacobs.sylvain@gmail.com>
2023-07-18 14:12:36 +0200

    personalised welcome back

--- Resources/Include/header_dashboard.php
+++ Resources/Include/header_dashboard.php
@@ -6,7 +6,12 @@
         </div>
         <div class="section__welcome__client">
             <div class="welcome__client__text">
-                <h2>Welcome back Henry</h2>
+                <?php session_start();
+                if (isset($_SESSION['user_name'])): ?>
+                    <h2>Welcome back
+                        <?php echo $_SESSION['user_name'] ?>!
+                    </h2>
+                <?php endif ?>
                 <p>You can here add an invoice, a company and some contacts</p>
             </div>
             <div class="welcome__client__image">
