8503b887934455af392a7f3eca62dcf2595b2ff4
Abel Fortunati <abel.fortunati@gmail.com>
2023-07-18 10:00:58 +0200

    form style

--- Resources/views/new_company.php
+++ Resources/views/new_company.php
@@ -6,29 +6,41 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="assets/css/reset.css" rel="stylesheet" type="text/css">
     <link href="assets/css/dashboard_nav.css" rel="stylesheet" type="text/css">
-    <script type="module" rel='javascript' src="assets/js/company_form.js"></script>
+    <link href="assets/css/dashboard_header.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/dashboard_tab.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/dashboard_main.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/dashboard_form.css" rel="stylesheet" type="text/css">
     <title>New Company</title>
 </head>
 
 <body>
-    <h1>New Company</h1>
-
+<?php
+     require '../Resources/Include/navbar_dashboard.php'
+    ?>
+    <main>
+        <?php
+            require '../Resources/Include/header_dashboard.php'
+        ?>
+    <div id="form_section">
+    <h3>New Company</h3>
+    <hr class="form_hr">
     <form action="/companies_add" method="POST">
-        <label for="name">Name:</label>
-        <input type="text" name="name" id="name" required><br>
+     <!--    <label for="name">Name:</label> -->
+        <input type="text" name="name" id="name" placeholder="Name" required><br>
 
-        <label for="type_id">Type:</label>
-        <input type="text" name="type_id" id="type_id" required><br>
+     <!--    <label for="type_id">Type:</label> -->
+        <input type="text" name="type_id" id="type_id" placeholder="Type" required><br>
 
-        <label for="country">Country:</label>
-        <input type="text" name="country" id="country" required><br>
+    <!--     <label for="country">Country:</label> -->
+        <input type="text" name="country" id="country" placeholder="Country" required><br>
 
-        <label for="tva">TVA:</label>
-        <input type="text" name="tva" id="tva" required><br>
+    <!--     <label for="tva">TVA:</label> -->
+        <input type="text" name="tva" id="tva" placeholder="TVA" required><br>
 
-        <button type="submit">Create</button>
+        <button type="submit">Save</button>
     </form>
-
+</div>
+</main>
 </body>
 
 </html>
--- Resources/views/new_contact.php
+++ Resources/views/new_contact.php
@@ -5,27 +5,39 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="assets/css/reset.css" rel="stylesheet" type="text/css">
     <link href="assets/css/dashboard_nav.css" rel="stylesheet" type="text/css">
-    <script type="module" rel='javascript' src="assets/js/contact_form.js"></script>
+    <link href="assets/css/dashboard_header.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/dashboard_tab.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/dashboard_main.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/dashboard_form.css" rel="stylesheet" type="text/css">
     <title>New Contact</title>
 </head>
 <body>
-<h1>New Contact</h1>
+<?php
+     require '../Resources/Include/navbar_dashboard.php'
+    ?>
+    <main>
+        <?php
+            require '../Resources/Include/header_dashboard.php'
+        ?>
+<div id="form_section">       
+    <h3>New Contact</h3>
+    <hr class="form_hr">
+    <form action="/contacts_add" method="POST">
+    <!-- <label for="name">Name:</label> -->
+        <input type="text" name="name" id="name" placeholder="Name" required><br>
 
-<form action="/contacts_add" method="POST">
-    <label for="name">Name:</label>
-    <input type="text" name="name" id="name" required><br>
+   <!--  <label for="company_id">Company:</label> -->
+        <input type="text" name="company_id" id="company_id" placeholder="Company"  required><br>
 
-    <label for="company_id">Company:</label>
-    <input type="text" name="company_id" id="company_id" required><br>
+   <!--  <label for="email">Email:</label> -->
+        <input type="email" name="email" id="email" placeholder="Email" required><br>
 
-    <label for="email">Email:</label>
-    <input type="email" name="email" id="email" required><br>
+  <!--   <label for="phone">Phone:</label> -->
+        <input type="text" name="phone" id="phone" placeholder="Phone" required><br>
 
-    <label for="phone">Phone:</label>
-    <input type="text" name="phone" id="phone" required><br>
-
-    <button type="submit">Create</button>
+        <button type="submit">Save</button>
 </form>
-
+</div>
+</main>
 </body>
 </html>
